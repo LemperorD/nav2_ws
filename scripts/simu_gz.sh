9752c45ca9a7b926435b3d74a6da9abca383308c
LD <ld2382619813@163.com>
2026-01-23 22:49:46 +0800

    feat(scripts): :zap: 新增三个仿真使用的脚本

---
+++ scripts/simu_gz.sh
@@ -0,0 +1,3 @@
+cd ~/nav2_ws
+source ./install/setup.bash
+ros2 launch rmu_gazebo_simulator bringup_sim.launch.py
---
+++ scripts/simu_nav.sh
@@ -0,0 +1,5 @@
+cd ~/nav2_ws
+source ./install/setup.bash
+ros2 launch pb2025_nav_bringup rm_navigation_simulation_launch.py \
+world:=rmul_2025 \
+slam:=False
