91da94ee9fe93db5347c4892f6eb81f52980337a
LD <ld2382619813@163.com>
2026-01-24 11:39:54 +0800

    feat(hikROS): :construction: 更新hikROS提交哈希，增加MVS启动脚本
    把mvs整套bin放进目录，在scripts文件夹加上运行脚本并写好路径
    
    实际运行效果待测试

---
+++ scripts/MVS.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ROOT_PATH=~/nav2_ws/src/hik_camera_ros2_driver/hikSDK/bin
+
+export LD_LIBRARY_PATH=${ROOT_PATH}:~/nav2_ws/src/hik_camera_ros2_driver/hikSDK/lib/amd64
+
+if [ "$EUID" -ne 0 ]; then
+  echo "not root user" 
+else
+  echo "Run as root"
+  chmod -R 777 ${ROOT_PATH}/Cfg
+  chmod -R 777 ${ROOT_PATH}/Temp
+fi
+
+exec ${ROOT_PATH}/MVS -platform xcb $1 $2
