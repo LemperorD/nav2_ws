386cac391a55a8c61f8bf9f3ff25cd5257d28581
LempeoroD <ld2382619813@163.com>
2026-02-05 16:05:40 +0800

    style(communication): :art: 增加头文件，改变终端输出字符的颜色
    为了方便调试，增加该头文件并更改通信中大量的打印函数
    
    待测试

---
+++ src/communication/include/communication/termcolor.hpp
@@ -0,0 +1,20 @@
+#ifndef COMMUNICATION_TERMCOLOR_HPP_
+#define COMMUNICATION_TERMCOLOR_HPP_
+
+#include <iostream>
+#include <sstream>
+
+namespace termcolor {
+  constexpr const char* reset   = "\033[0m";
+
+  constexpr const char* white   = "\033[37m";
+  constexpr const char* red     = "\033[31m";
+  constexpr const char* yellow  = "\033[33m";
+  constexpr const char* brown   = "\033[33m";
+  constexpr const char* green   = "\033[32m";
+  constexpr const char* blue    = "\033[34m";
+
+  constexpr const char* orange256 = "\033[38;5;208m";
+}
+
+#endif // COMMUNICATION_TERMCOLOR_HPP_
