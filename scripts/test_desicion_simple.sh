5fbc810d3b66d85434073734c2b8578ff6b0a881
HaoZhen Liu <t2684450263@outlook.com>
2026-04-30 00:46:05 +0800

    debug: do not use absolute path in test script

--- scripts/test_desicion_simple.sh
+++ scripts/test_desicion_simple.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 source /opt/ros/humble/setup.bash
-cd /home/rog/nav2_ws
+cd ~/nav2_ws
 
 echo "Building decision_simple..."
 colcon build --packages-select decision_simple --cmake-args -DBUILD_TESTING=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-. /home/rog/nav2_ws/install/setup.bash
-cd /home/rog/nav2_ws/build/decision_simple && ./test_general
-cd /home/rog/nav2_ws
+. ~/nav2_ws/install/setup.bash
+cd ~/nav2_ws/build/decision_simple && ./test_general
+cd ~/nav2_ws
